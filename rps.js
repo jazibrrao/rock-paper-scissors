ac7acf9ab2b270fe676aa883eb92b7e76fde312c
Jazz <jazibrrao@gmail.com>
2022-11-01 14:58:19 -0400

    add logic for winning and losing conditions

--- rps.js
+++ rps.js
@@ -1,7 +1,11 @@
-const choices = ['rock', 'paper', 'scissors'];
+let playerScore = 0;
+let computerScore = 0;
+let roundNumber = 0;
+let playerSelection = 'rock';
 
 function getComputerChoice() {
     // randomly return 'Rock', 'Paper', 'Scissors'
+    const choices = ['rock', 'paper', 'scissors'];
     const randomChoice = Math.floor(Math.random() * choices.length);
 
     let computerSelection = choices[randomChoice];
@@ -13,18 +17,41 @@ function playRound(playerSelection, computerSelection) {
     // prompt user for their choice (case-insensitive)
     // player vs computer
     // return string that declares winner
-    playerSelection = prompt("Please type in your choice (Rock, Paper or Scissors)").toLowerCase();
-     
-    switch(playerSelection) {
-        case ('rock' && computerSelection == 'rock'):
-            console.log(`It's a tie!`);
-            break;
-        
+    //playerSelection = prompt("Type in your choice: (Rock, Paper or Scissors)").toLowerCase();
+    
+    if (playerSelection === computerSelection) {
+        console.log(`It's a tie!`);
+    }
+    else if (playerSelection == 'rock') {
+        if (computerSelection == 'paper') {
+            console.log('The computer chose paper. You lose this round!');
+        }
+        else {
+            console.log('The computer chose scissors. You win this round!');
+        }
+    }
+    else if (playerSelection == 'paper') {
+        if (computerSelection == 'rock') {
+            console.log('The computer chose rock. You win this round!');
+        }
+        else {
+            console.log('The computer chose scissors. You lose this round!');
+        }
+    }
+    else {
+        if (computerSelection == 'rock') {
+            console.log('The computer chose rock. You lose this round!');
+        }
+        else {
+            console.log('The computer chose paper. You win this round!');
+        }
     }
 }
+    
 
 function game() {
     // play    
 }
 
 getComputerChoice();
+playRound(playerSelection, getComputerChoice());
