dd6c0ac4f6479a396957a299525403300fea60aa
Jazz <jazibrrao@gmail.com>
2022-11-01 16:21:57 -0400

    add game function to play rounds until winner is decided

--- rps.js
+++ rps.js
@@ -1,7 +1,6 @@
 let playerScore = 0;
 let computerScore = 0;
-let roundNumber = 0;
-let playerSelection = 'rock';
+let playerSelection = 'scissors';
 
 function getComputerChoice() {
     // randomly return 'Rock', 'Paper', 'Scissors'
@@ -16,6 +15,8 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     // prompt user for their choice (case-insensitive)
     // return string that declares winner
+    // track computer/player wins in global variables
+    
     //playerSelection = prompt("Type in your choice: (Rock, Paper or Scissors)").toLowerCase();
     
     if (playerSelection === computerSelection) {
@@ -23,32 +24,47 @@ function playRound(playerSelection, computerSelection) {
     }
     else if (playerSelection == 'rock') {
         if (computerSelection == 'paper') {
+            computerScore += 1;
             return 'You Lose! Paper beats Rock.';
         }
         else {
+            playerScore += 1;
             return 'You Win! Rock beats Scissors.';
         }
     }
     else if (playerSelection == 'paper') {
         if (computerSelection == 'rock') {
+            playerScore += 1;
             return 'You Win! Paper beats Rock.';
         }
         else {
+            computerScore += 1;
             return 'You Lose! Scissors beats Paper.';
         }
     }
     else {
         if (computerSelection == 'rock') {
+            computerScore += 1;
             return 'You Lose! Rock beats Scissors.';
         }
         else {
+            playerScore += 1;
             return 'You Win! Scissors beats Paper.';
         }
     }
 }
     
 function game() {
-    // play    
+    // keep playing until either player or computer hit 5 wins
+    while (playerScore < 5 && computerScore < 5) {
+        console.log(playRound(playerSelection, getComputerChoice()));
+    }
+    if (playerScore > computerScore) {
+        console.log(`You Won! Final Score: ${playerScore} - ${computerScore}`);
+    }
+    else {
+        console.log(`You Lost! Final Score: ${playerScore} - ${computerScore}`);
+    }
 }
 
-console.log(playRound(playerSelection, getComputerChoice()));
+game();
